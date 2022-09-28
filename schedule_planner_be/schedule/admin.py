8d94b711b4b5970645a5fb25f6a4e3545cccf371
MyshkavetsSviatlana <myshkavets.sviatlana@gmail.com>
2022-09-28 17:54:56 +0300

    add 'is_active' to Classroom, delete possibility to delete classrooms from DB.

--- schedule_planner_be/schedule/admin.py
+++ schedule_planner_be/schedule/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import Location, SubwayStation, Classroom, Schedule
 
-admin.site.register(Location)
+
 admin.site.register(SubwayStation)
 admin.site.register(Schedule)
-admin.site.register(Classroom)
+admin.site.register(Location)
 # admin.site.register(Availability)
 # admin.site.register(AvailabilityOccurrence)
 # admin.site.register(ClassroomReservation)
 # admin.site.register(TimeSlot)
 
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['id', "classroom", "location", "is_active"]
+    ordering = ["-id"]
+    search_fields = ["location"]
+    list_filter = ["location", "is_active"]
 
 
